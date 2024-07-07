1ae8d5b0f0d36145e2a0106ed9f65ca790a904a9
Alexander Vinogradow <alukard981@gmail.com>
2024-07-07 19:33:25 +0300

    gradle trash added

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+    jacoco
+    id("checkstyle")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.mrsarm.jshell.plugin") version "1.2.1"
+    id("com.github.ben-manes.versions") version "0.50.0"
+
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.compileJava {
+    options.release = 21
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
