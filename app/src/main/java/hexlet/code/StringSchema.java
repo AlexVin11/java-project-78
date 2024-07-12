2f49e242ee787a8bad9a282c4a8bdc7dfbc47674
Alexander Vinogradow <alukard981@gmail.com>
2024-07-12 18:34:32 +0300

    minor changes in StringSchema (step 1)

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -4,37 +4,24 @@ package hexlet.code;
 import java.util.function.Predicate;
 
 public class StringSchema extends BasicSchema {
-    private Boolean isRequired;
-    private Integer minLength;
-    private String contains;
+    private final String REQUIRED = "required";
+    private final String MIN_LENGTH = "minLength";
 
-    public boolean isRequired() {
-        this.isRequired = true;
-        Predicate<StringSchema> isRequired = this -> {
-            if (this.isRequired == null) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        this.checks.put("isReuided", isRequired);
+    public void isRequired() {
+        this.required = true;
     }
 
     public void minLength(int number) {
-        this.minLength = number;
+        Predicate<BasicSchema> minLengthPredicate = e ->
+        this.checks.put(MIN_LENGTH, )
     }
 
     public void contains(String str) {
-        this.contains += str;
+
     }
 
     @Override
     public boolean isValid() {
-        if (this.checks.isEmpty()) {
-            return true;
-        } else if (this.checks.containsKey("isRequired")) {
-
-        }
         return false;
     }
 }
