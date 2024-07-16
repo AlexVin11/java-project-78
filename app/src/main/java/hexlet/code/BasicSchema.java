e834a4560c19dfbe1d0289b7775da9a08cd05a4f
Alexander Vinogradow <alukard981@gmail.com>
2024-07-16 18:23:04 +0300

    1. There is no need to hande with checks as Map (contain a name of the check and predicate itself) so map was removed and LinkedList created. 2. addCheck method changed to handle with list 3. method setRequired property added 4. isValid method now made as jeneric (T t as input).

--- app/src/main/java/hexlet/code/BasicSchema.java
+++ app/src/main/java/hexlet/code/BasicSchema.java
@@ -1,18 +1,21 @@
 package hexlet.code;
 
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public abstract class BasicSchema<T> {
-    protected Map<String, Predicate<T>> checks = new LinkedHashMap<>();
+    protected List<Predicate<T>> checks = new LinkedList<>();
     protected boolean required = false;
 
-    protected final void addCheck(String checkName, Predicate<T> predicate) {
-        checks.put(checkName, predicate);
+    protected final void addCheck(Predicate<T> predicate) {
+        checks.add(predicate);
     }
 
-    public abstract boolean isValid(Objects obj);
+    public void setRequired() {
+        this.required = true;
+    }
+
+    public abstract boolean isValid(T t);
 }
