58735d5f4f04799a096229a816fb478a60efa1d6
Alexander Vinogradow <alukard981@gmail.com>
2024-07-17 17:44:29 +0300

    In BasicSchema class Linked list changed to Array List - i dont understand yet if i need to have an order of added checks

--- app/src/main/java/hexlet/code/BasicSchema.java
+++ app/src/main/java/hexlet/code/BasicSchema.java
@@ -1,15 +1,20 @@
 package hexlet.code;
 
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public abstract class BasicSchema<T> {
-    protected List<Predicate<T>> checks = new LinkedList<>();
+    protected List<Predicate<T>> checks = new ArrayList<>();
     protected boolean required = false;
 
     protected final void addCheck(Predicate<T> predicate) {
+        checks.add(predicate);
+    }
+
+    protected final void updateCheck(Predicate<T> predicate) {
+        checks.clear();
         checks.add(predicate);
     }
 
