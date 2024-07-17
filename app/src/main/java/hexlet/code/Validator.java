801a733b0055b5ca517643330eb2498ae221ee3a
Alexander Vinogradow <alukard981@gmail.com>
2024-07-17 17:54:33 +0300

    I guess there is no need to have a list of schemas

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -4,12 +4,9 @@ package hexlet.code;
 import java.util.ArrayList;
 
 public class Validator {
-    //список созданных схем валидации
-    ArrayList<BasicSchema> schemas;
 
-    public StringSchema string() {
+    public static StringSchema string() {
         var createdSchema = new StringSchema();
-        schemas.add(createdSchema);
         return createdSchema;
     }
 }
