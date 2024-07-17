50d501f2a0961c1a2f1e38444090ce002c272ffc
Alexander Vinogradow <alukard981@gmail.com>
2024-07-17 17:53:57 +0300

    Testing methods created for isValid method

---
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringSchemaTest {
+    private static StringSchema stringSchema;
+
+    @BeforeAll
+    public static void beforeAll() {
+        stringSchema = Validator.string();
+    }
+
+    @Test
+    public void basicTestIsValid() {
+        assertEquals(true, stringSchema.isValid(""));
+        assertEquals(true, stringSchema.isValid(null));
+        assertEquals(true, stringSchema.isValid("what does the fox say"));
+        assertEquals(true, stringSchema.isValid("hexlet"));
+    }
+
+    @Test
+    public void conatainsTestIsValid() {
+        stringSchema = stringSchema.contains("wh");
+        assertEquals(true, stringSchema.isValid("what does the fox say"));
+        stringSchema = stringSchema.contains("what");
+        assertEquals(true, stringSchema.isValid("what does the fox say"));
+        stringSchema = stringSchema.contains("whatthe");
+        assertEquals(false, stringSchema.isValid("what does the fox say"));
+    }
+}
