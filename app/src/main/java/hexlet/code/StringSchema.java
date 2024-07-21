6271e5c64eab04b42fe0fa6f0caca14c0b70c108
Alexander Vinogradow <alukard981@gmail.com>
2024-07-21 21:37:24 +0300

    StringSchema step 3

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,39 +1,39 @@
 package hexlet.code;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class StringSchema extends BasicSchema<String> {
+    private final String MIN_LENGTH_CHECK_NAME = "minLength";
+    private final String CONTAINS_CHECK_NAME = "contains";
 
     public StringSchema required() {
-        setRequired();
+        Predicate<String> requiredPredicate = e -> !Objects.isNull(e) && !e.equals("");
+        if (this.checks.containsKey(IS_REQUIRED)) {
+            updateCheck(IS_REQUIRED, requiredPredicate);
+        } else {
+            this.addCheck(IS_REQUIRED, requiredPredicate);
+        }
         return this;
     }
 
     public StringSchema minLength(int number) {
-        Predicate<String> minLength = e -> e.length() >= number;
-        updateCheck(minLength);
+        Predicate<String> isMinLength = e -> e.length() >= number;
+        if (this.checks.containsKey(MIN_LENGTH_CHECK_NAME)) {
+            updateCheck(MIN_LENGTH_CHECK_NAME, isMinLength);
+        } else {
+            this.addCheck(MIN_LENGTH_CHECK_NAME, isMinLength);
+        }
         return this;
     }
 
     public StringSchema contains(String str) {
-        Predicate<String> contains = e -> e.contains(str);
-        updateCheck(contains);
-        return this;
-    }
-
-    @Override
-    public boolean isValid(String str) {
-        if (this.required == false) {
-            return true;
+        Predicate<String> containsPredicate = e -> e.contains(str);
+        if (this.checks.containsKey(CONTAINS_CHECK_NAME)) {
+            updateCheck(CONTAINS_CHECK_NAME, containsPredicate);
         } else {
-            for (var check : this.checks) {
-                if (!check.test(str)) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
+            this.addCheck(CONTAINS_CHECK_NAME, containsPredicate);
         }
-        return false;
+        return this;
     }
 }
