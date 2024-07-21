d179c59b72be784fa86c96ca32bb5982fc4d2c3f
Alexander Vinogradow <alukard981@gmail.com>
2024-07-21 21:37:32 +0300

    StringSchemaTest step 3

--- app/src/test/java/hexlet/code/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -14,7 +14,7 @@ public class StringSchemaTest {
     }
 
     @Test
-    public void basicTestIsValid() {
+    public void nullableTestIsValid() {
         assertEquals(true, stringSchema.isValid(""));
         assertEquals(true, stringSchema.isValid(null));
         assertEquals(true, stringSchema.isValid("what does the fox say"));
@@ -27,6 +27,7 @@ public class StringSchemaTest {
     @Test
     public void conatainsTestIsValid() {
         stringSchema.required();
+        assertEquals(true, stringSchema.isValid("what does the fox say"));
         stringSchema = stringSchema.contains("wh");
         assertEquals(true, stringSchema.isValid("what does the fox say"));
         stringSchema = stringSchema.contains("what");
@@ -39,6 +40,14 @@ public class StringSchemaTest {
     public void testMinLength() {
         stringSchema.required();
         stringSchema.minLength(10).minLength(4);
-        assertEquals(true, stringSchema.isValid("Hexlet"));
+        assertEquals(true, stringSchema.isValid("Hexl"));
+    }
+
+    @Test
+    public void complexTestIsValid() {
+        stringSchema.required().minLength(5).minLength(4).contains("hex");
+        assertEquals(false, stringSchema.isValid("hex"));
+        assertEquals(false, stringSchema.isValid("mako"));
+        assertEquals(true, stringSchema.isValid("hexl"));
     }
 }
