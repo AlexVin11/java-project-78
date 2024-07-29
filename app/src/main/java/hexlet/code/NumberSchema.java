4e7fa19fd27ab24fa74f2aac42861e77cd645822
Alexander Vinogradow <alukard981@gmail.com>
2024-07-29 20:01:06 +0300

    NumberSchema checkstyle refactor

--- app/src/main/java/hexlet/code/NumberSchema.java
+++ app/src/main/java/hexlet/code/NumberSchema.java
@@ -3,9 +3,9 @@ package hexlet.code;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public class NumberSchema extends BasicSchema<Integer>{
-    private final String NUMBER_IS_POSITIVE = "positive";
-    private final String NUMBER_IN_RANGE = "range";
+public class NumberSchema extends BasicSchema<Integer> {
+    private final String numberIsPositive = "positive";
+    private final String numberInRange = "range";
 
     public NumberSchema required() {
         Predicate<Integer> requiredPredicate = e -> !Objects.isNull(e);
@@ -17,19 +17,20 @@ public class NumberSchema extends BasicSchema<Integer>{
         Predicate<Integer> positive = e -> {
             if (e == 0) {
                 return false;
-            } else if (e > 0) {
+            }
+            if (e > 0) {
                 return true;
             } else {
                 return false;
             }
         };
-        addCheck(NUMBER_IS_POSITIVE, positive);
+        addCheck(numberIsPositive, positive);
         return this;
     }
 
     public NumberSchema range(Integer a, Integer b) {
         Predicate<Integer> rangePredicate = e -> e >= a && e <= b;
-        addCheck(NUMBER_IN_RANGE, rangePredicate);
+        addCheck(numberInRange, rangePredicate);
         return this;
     }
 }
