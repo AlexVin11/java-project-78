d2e6f68b6e6a5ac9bcd4e699096b4ef05e98fefc
Alexander Vinogradow <alukard981@gmail.com>
2024-07-29 19:53:46 +0300

    StringSchema checkstyle refactor

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 import java.util.function.Predicate;
 
 public class StringSchema extends BasicSchema<String> {
-    private final String MIN_LENGTH_CHECK_NAME = "minLength";
-    private final String CONTAINS_CHECK_NAME = "contains";
+    private final String minLengthCheckName = "minLength";
+    private final String containsCheckName = "contains";
 
     public StringSchema required() {
         Predicate<String> requiredPredicate = e -> !Objects.isNull(e) && !e.equals("");
@@ -15,13 +15,13 @@ public class StringSchema extends BasicSchema<String> {
 
     public StringSchema minLength(int number) {
         Predicate<String> isMinLength = e -> e.length() >= number;
-        this.addCheck(MIN_LENGTH_CHECK_NAME, isMinLength);
+        this.addCheck(minLengthCheckName, isMinLength);
         return this;
     }
 
     public StringSchema contains(String str) {
         Predicate<String> containsPredicate = e -> e.contains(str);
-        this.addCheck(CONTAINS_CHECK_NAME, containsPredicate);
+        this.addCheck(containsCheckName, containsPredicate);
         return this;
     }
 }
