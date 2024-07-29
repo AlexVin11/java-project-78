1ccd98d70b7b5e187171b9b27dc233239daacc89
Alexander Vinogradow <alukard981@gmail.com>
2024-07-29 19:28:03 +0300

    StringSchema step 5 refactor- updateCheck removed.

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -9,31 +9,19 @@ public class StringSchema extends BasicSchema<String> {
 
     public StringSchema required() {
         Predicate<String> requiredPredicate = e -> !Objects.isNull(e) && !e.equals("");
-        if (this.checks.containsKey(IS_REQUIRED)) {
-            updateCheck(IS_REQUIRED, requiredPredicate);
-        } else {
-            this.addCheck(IS_REQUIRED, requiredPredicate);
-        }
+        this.addCheck(IS_REQUIRED, requiredPredicate);
         return this;
     }
 
     public StringSchema minLength(int number) {
         Predicate<String> isMinLength = e -> e.length() >= number;
-        if (this.checks.containsKey(MIN_LENGTH_CHECK_NAME)) {
-            updateCheck(MIN_LENGTH_CHECK_NAME, isMinLength);
-        } else {
-            this.addCheck(MIN_LENGTH_CHECK_NAME, isMinLength);
-        }
+        this.addCheck(MIN_LENGTH_CHECK_NAME, isMinLength);
         return this;
     }
 
     public StringSchema contains(String str) {
         Predicate<String> containsPredicate = e -> e.contains(str);
-        if (this.checks.containsKey(CONTAINS_CHECK_NAME)) {
-            updateCheck(CONTAINS_CHECK_NAME, containsPredicate);
-        } else {
-            this.addCheck(CONTAINS_CHECK_NAME, containsPredicate);
-        }
+        this.addCheck(CONTAINS_CHECK_NAME, containsPredicate);
         return this;
     }
 }
